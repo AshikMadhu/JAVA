6dca2ea4913845e1f1fa740ba8bfd89158936f68
Ashik Madhu <ashiksjc2025@gmail.com>
2026-07-23 22:48:49 +0530

    Add files via upload

---
+++ java_programs/even100.java
@@ -0,0 +1,17 @@
+package java_programs;
+
+public class even100 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Even Numbers Between 1 and 100:");
+
+        for (int i = 2; i <= 100; i += 2) {
+
+            System.out.print(i + " ");
+
+        }
+
+    }
+
+}
