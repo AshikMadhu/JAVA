2b5ac340d86eb55d18e4bece88ee0cb4a39ee461
Ashik Madhu <ashiksjc2025@gmail.com>
2026-07-23 23:06:02 +0530

    Add files via upload

---
+++ java_programs/GCD.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class GCD {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter First Number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter Second Number: ");
+        int b = sc.nextInt();
+
+        while (b != 0) {
+
+            int temp = b;
+
+            b = a % b;
+
+            a = temp;
+
+        }
+
+        System.out.println("GCD = " + a);
+
+        sc.close();
+
+    }
+
+}
