7bd887e1faf68e96382cdef0e8ad1745c2faeb94
Ashik Madhu <ashiksjc2025@gmail.com>
2026-08-02 13:58:10 +0530

    Add files via upload

---
+++ javatutorial/EvenNumbers.java
@@ -0,0 +1,15 @@
+package tutorials;
+
+public class EvenNumbers {
+
+    public static void main(String[] args) {
+
+        int i = 2;
+
+        do {
+            System.out.println(i);
+            i = i + 2;
+        } while (i <= 20);
+
+    }
+}
